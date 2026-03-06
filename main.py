7dc70497057acac85f7b2675e83998b447d306ca
Louis Héraut <louis.heraut@inrae.fr>
2026-03-06 03:47:46 +0100

    debug

--- main.py
+++ main.py
@@ -125,11 +125,7 @@ def main():
     if args.all or args.download:
         downloaded_files =  download(STATE_FILE, DOWNLOAD_DIR,
                                      METEO_BASE_URL, METEO_DATASET_ID)
-
-        print(downloaded_files)
-        print(args.download)
-        
-        if not downloaded_files and args.download:
+        if not downloaded_files:
             print("\n✨ Rien de nouveau à télécharger!")
             return
 
