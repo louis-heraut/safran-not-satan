150797567626ee67b333b99382ffcb6789e63551
Louis Héraut <louis.heraut@inrae.fr>
2026-03-10 18:32:22 +0100

    debug

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ def main():
                               S3_ACCESS_KEY=S3_ACCESS_KEY,
                               S3_SECRET_KEY=S3_SECRET_KEY,
                               S3_ENDPOINT=S3_ENDPOINT,
-                              S3_REGION=S3_REGION):
+                              S3_REGION=S3_REGION)
 
         upload_s3(S3_BUCKET=S3_BUCKET,
                   S3_PREFIX=S3_PREFIX,
